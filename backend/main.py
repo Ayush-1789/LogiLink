97165ffefa72b6f5adaa3f30fccf8b0e7bff8626
Vacha <vachakb@gmail.com>
2025-03-05 19:56:54 +0100

    use lifespan function instead of startup event and add /routes endpoint (wip)

--- backend/main.py
+++ backend/main.py
@@ -1,23 +1,40 @@
+from contextlib import asynccontextmanager
 from typing import Annotated, List
-from fastapi import Depends, FastAPI
-from sqlalchemy import select
+from fastapi import Depends, FastAPI, Query
+from sqlalchemy import select, func
 from sqlalchemy.ext.asyncio import AsyncSession
 from database import create_tables, get_db
 from models import Route
 from dtos import RouteDTO
 
-app = FastAPI()
-
 SessionDep = Annotated[AsyncSession, Depends(get_db)]
 
-
-@app.on_event("startup")
-async def on_startup():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     await create_tables()
+    yield
 
 
-@app.get("/", response_model=List[RouteDTO])
-async def index(session: SessionDep):
-    results = await session.execute(select(Route))
-    routes = results.scalars().all()
-    return routes
+app = FastAPI(lifespan=lifespan)
+    
+
+def get_point(value: str) -> str:
+    lat_str, lon_str = value.split(',')
+    latitude = float(lat_str)
+    longitude = float(lon_str)
+    return f"SRID=4326;POINT({longitude} {latitude})"
+
+
+@app.get("/routes", response_model=List[RouteDTO])
+async def index(session: SessionDep,
+                to: str,
+                from_param: str = Query(alias="from")):
+    from_point = get_point(from_param)
+
+    stmt = select(Route).order_by(
+        func.ST_Distance(Route.start_point, func.ST_GeomFromText(from_point, 4326))
+    ).limit(3)
+
+    result = await session.execute(stmt)
+
+    return result.scalars().all()
